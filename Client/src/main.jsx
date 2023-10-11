f2b8c7c754edca4d8c65a71f1adbc4552b7a0767
Shincy <shincyshnz@gmail.com>
2023-10-11 10:56:08 +0530

    App wrapped by react-query provider

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,12 +4,17 @@ import App from "./App.jsx";
 import "./index.css";
 import { ErrorProvider } from "./context/errorContext.jsx";
 import { TodoProvider } from "./context/todoContext.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorProvider>
       <TodoProvider>
+        <QueryClientProvider client={queryClient}>
           <App />
+        </QueryClientProvider>
       </TodoProvider>
     </ErrorProvider>
   </React.StrictMode>
