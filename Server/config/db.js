999aca806d70d7ab743d3fb4192c48f675ac5b0c
Shincy <shincyshnz@gmail.com>
2024-01-17 10:23:10 +0400

    modified

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
     try {
-        const { connection } = await mongoose.connect("mongodb://127.0.0.1:27017/todoapp");
+        const { connection } = await mongoose.connect(process.env.MONGO_URL);
         console.log("Database Connected : " + connection.host);
     } catch (error) {
         console.log(error.message);
--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDb = require("./config/db");
 const cors = require("cors");
+require('dotenv').config();
 const todoModel = require("./models/todoModel");
 const app = express();
 
